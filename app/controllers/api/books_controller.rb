905b64b79976d81e9507812e37dafdf028b40c4b
Miller Arlindo Hall <hall@hey.com>
2023-01-30 20:10:45 -0500

    Add book index and todos for MVP (bin/rails notes)

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,2 +1,12 @@
-class Api::BooksController < ApplicationController
+module Api
+  class BooksController < ApplicationController
+    # TODO: create books
+    # TODO: helper to search for books in the library of congress
+
+    def index
+      # TODO: authenticate
+      # TODO: paginate
+      render json: Book.all
+    end
+  end
 end
