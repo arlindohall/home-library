e9200a3e78f24787fd1babee45535100a70cdc8e
Miller Arlindo Hall <hall@hey.com>
2023-01-31 10:38:06 -0500

    Create frontend structure using react router

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,14 +1,27 @@
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Books } from './books';
+import { NotFound } from './lib';
 
 // App structure taken from https://blog.dennisokeeffe.com/blog/2022-02-19-rails-7-using-react-with-esbuildom
 
-const App = () => {
-  return <div>Hello, world!</div>
-}
+const App = () => (
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/books" element={<Books/>}/>
+        <Route path="/" element={<Books/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
+  </div>
+);
 
 document.addEventListener("DOMContentLoaded", () => {
-  const root = document.getElementById("root");
-  ReactDOM.render(<App />, root);
+  const container = document.getElementById('root');
+  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  root.render(<App/>);
 })
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   end
 
   root "static_pages#root"
+
+  get "*path", to: "static_pages#root"
 end
