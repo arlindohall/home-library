07ac39bdcd0a283f1b0945db77c1a561b5326bdf
Miller Arlindo Hall <hall@hey.com>
2023-02-03 22:03:11 -0500

    Change production logger to syslog anticipating docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,15 +79,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  # Use system log so docker container cleanup also gets /var/log
+  require "syslog/logger"
+  config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "home-library")
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
