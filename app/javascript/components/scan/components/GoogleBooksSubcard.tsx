836b21fd89a7af67e6f0eeb1b5ae7e900bbfe8e2
Miller Arlindo Hall <hall@hey.com>
2023-02-06 21:00:25 -0500

    Don't submit when clicking google link

--- app/javascript/components/scan/components/GoogleBooksSubcard.tsx
+++ app/javascript/components/scan/components/GoogleBooksSubcard.tsx
@@ -33,8 +33,12 @@ export default ({
           <Typography variant="body2">Author: {author}</Typography>
           <Typography variant="body2">Description: {description}</Typography>
           <Typography variant="body2">ISBN: {isbn}</Typography>
-          <Typography variant="body2">Google Books Link: <Link href={googleBooksLink}>{googleBooksLink}</Link></Typography>
-          <Typography variant="body2">Google Books API Link: <Link href={googleBooksApiLink}>{googleBooksApiLink}</Link></Typography>
+          <Typography variant="body2" onClick={(e) => {e.stopPropagation()}}>
+            Google Books Link: <Link href={googleBooksLink}>{googleBooksLink}</Link>
+          </Typography>
+          <Typography variant="body2" onClick={(e) => {e.stopPropagation()}}>
+            Google Books API Link: <Link href={googleBooksApiLink}>{googleBooksApiLink}</Link>
+          </Typography>
         </CardContent>
       </Card>
     </>
