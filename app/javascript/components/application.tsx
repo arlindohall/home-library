538d9f290691f00ffd3a6f152be1135cbf596fc9
Miller Arlindo Hall <hall@hey.com>
2023-02-08 14:13:49 -0500

    Add menu bar and fix extra css margin

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -6,11 +6,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Books, Book } from './books';
 import { Scan, ScanDetails } from './scan';
 import { NotFound } from './lib';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // App structure taken from https://blog.dennisokeeffe.com/blog/2022-02-19-rails-7-using-react-with-esbuildom
 
 const App = () => (
-  <div>
+  <>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Home Library
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </Box>
     <Router>
       <Routes>
         <Route path="/scan" element={<Scan/>}/>
@@ -22,7 +43,7 @@ const App = () => (
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </Router>
-  </div>
+  </>
 );
 
 document.addEventListener("DOMContentLoaded", () => {
