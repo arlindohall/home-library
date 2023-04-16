c538db9088f21d05686f8e6c478b6bad3b51b7e9
Miller Arlindo Hall <hall@hey.com>
2023-04-16 15:26:22 -0400

    Add automatic session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
